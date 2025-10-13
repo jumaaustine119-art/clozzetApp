b052bfcd476d125e29f71390c3b5414875e997a8
jumaaustine <jumaaustine119@gmail.com>
2025-10-13 07:15:52 +0300

    made changes

--- clozzetApp/admin.py
+++ clozzetApp/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Category, Product
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product)a
